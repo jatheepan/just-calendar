ad0b9ec83a053453c3b2b2379da60f04d31bbcc2
SergeyCherepanov <sergey@cherepanov.org.ua>
2013-03-22 18:52:10 +0200

    Demo update.

--- examples/demo.js
+++ examples/demo.js
@@ -13,3 +13,29 @@ new JustCalendar({
     startDate: new Date(),
     calendars: 2
 });
+
+//Paint even days
+
+new JustCalendar({
+    container: document.getElementById('calendar-example-paint'),
+    startDate: new Date(),
+    calendars: 2,
+    onRender: function(table, date) {
+        this.options.container.style.background = "#eeeeee"; 
+    },
+    onCellRender:    function(cellContainer, cellDate, scopeDate) {
+        if (!(cellDate.getDate().toString()%2)) {
+            cellContainer.style.background = "#000000";
+            cellContainer.style.color = "#ffffff";
+            cellContainer.onclick = function() {
+                alert("Black");
+            }
+        } else {
+            cellContainer.style.background = "#cccccc";
+            cellContainer.style.color = "#000000";
+            cellContainer.onclick = function() {
+                alert("Gray");
+            }
+        }
+    }
+});
